66a5f7a1a802f4a9a3a35dd5ee101ee3074a453f
cyper <345343747@qq.com>
2017-03-06 00:05:25 +0800

    chapter 8 orders and administration

--- app/controllers/sportsStore.js
+++ app/controllers/sportsStore.js
@@ -1,6 +1,7 @@
 angular.module('sportsStore')
     .constant('dataUrl', "http://localhost:2403/products")
-    .controller('sportsStoreCtrl', function ($scope, $http, dataUrl) {
+    .constant('orderUrl', "http://localhost:2403/orders")
+    .controller('sportsStoreCtrl', function ($scope, $http, $location, dataUrl, orderUrl, cart) {
         $scope.data = {
             // products: [
             //     {name: "Product #1", description: "A product", category: "Category #1", price: 100},
@@ -16,4 +17,20 @@ angular.module('sportsStore')
             .error(function (error) {
                 $scope.data.error = error;
             });
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                })
+                .finally(function () {
+                    $location.path('/complete');
+                })
+        };
     });
